8a28c707523349c36a045aab3baec63205ac41fd
emys <emys@mail.ru>
2022-08-14 14:19:54 +0300

    even_numbers

--- tasks/easy/generators/even_numbers.py
+++ tasks/easy/generators/even_numbers.py
@@ -9,3 +9,19 @@ next(even_gen) -> 2
 next(even_gen) -> 4
 next(even_gen) -> 6
 """
+
+
+def get_even_number():
+    n = 1
+    while True:
+        yield n + 1
+        n += 2
+
+
+even_gen = get_even_number()
+
+
+print(next(even_gen))
+print(next(even_gen))
+print(next(even_gen))
+print(next(even_gen))
